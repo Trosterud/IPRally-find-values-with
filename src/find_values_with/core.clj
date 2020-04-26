664968d49118bad287cf11588ecbd2ec76e2e695
Sindre Trosterud <sindre.reino.trosterud@gmail.com>
2020-04-26 12:53:51 +0300

    refactor main with beautiful thread last

--- src/find_values_with/core.clj
+++ src/find_values_with/core.clj
@@ -6,6 +6,6 @@
   "Returns parsed JSON from file, takes filepath string as input"
   [json-file-path]
   (let [get-parsed-json (json/parse-string (slurp json-file-path) true)]
-    (map :value
-         (filter map?
-                 (tree-seq #(or (map? %) (vector? %)) identity (first get-parsed-json))))))
+    (->> (tree-seq #(or (map? %) (vector? %)) identity (first get-parsed-json))
+         (filter map?)
+         (map :value))))
