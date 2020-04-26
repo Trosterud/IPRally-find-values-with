8e551c9d8cb657cc3ce631642fd1426b7cf72303
Sindre Trosterud <sindre.reino.trosterud@gmail.com>
2020-04-26 17:50:33 +0300

    add regex matching as documentation

--- src/find_values_with/core.clj
+++ src/find_values_with/core.clj
@@ -9,7 +9,7 @@
 
   [json-file-path key substring]
   (let [get-parsed-json (json/parse-string (slurp json-file-path) true)
-        substring-match? (fn [s] (str/includes? s substring))]
+        substring-match? (fn [s] (str/includes? s substring))] ; or w/regex matching (boolean (re-find #"motor" s))
 
     ;; Perform depth first search, filter for relevant objects, map :value
     (->> (tree-seq #(or (map? %) (vector? %)) identity (first get-parsed-json))
