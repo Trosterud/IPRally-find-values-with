0d40324eacd1aaa728eab5c76c9f612a8d937810
Sindre Trosterud <sindre.reino.trosterud@gmail.com>
2020-04-26 18:14:44 +0300

    add arg validation and fix return type to Vector

--- src/find_values_with/core.clj
+++ src/find_values_with/core.clj
@@ -1,13 +1,17 @@
 (ns find-values-with.core
   (:require [cheshire.core :as json]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clojure.java.io :as io])
   (:gen-class))
 
 (defn -main
-  "Returns a list of the values containing the given substring from all key's 'value' from the given JSON file. 
-   As input it takes the file path to the JSON file and the string to match values with."
+  "Returns a vector of the values containing the arg substring,
+   from all key's matching the arg key, in the given JSON file."
 
   [json-file-path key substring]
+  {:pre [(.exists (io/file json-file-path)) (string? key) (string? substring)]
+   :post [(= (type %) clojure.lang.PersistentVector)]}
+
   (let [get-parsed-json (json/parse-string (slurp json-file-path) true)
         substring-match? (fn [s] (str/includes? s substring))] ; or w/regex matching (boolean (re-find #"motor" s))
 
@@ -17,4 +21,4 @@
     ;; 3. And lastly we map (verb) and return these filtered string values
     (->> (tree-seq #(or (map? %) (vector? %)) identity (first get-parsed-json))
          (filter #(and (map? %) ((keyword key) %) (substring-match? (get % (keyword key)))))
-         (map (keyword key)))))
+         (mapv (keyword key))))) ; mapv turns the LazySeq into a Vector, without using Into
