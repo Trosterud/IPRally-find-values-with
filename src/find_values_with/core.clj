f822503665c5a01c2446f763edbad2b432174ad7
Sindre Trosterud <sindre.reino.trosterud@gmail.com>
2020-04-26 12:39:09 +0300

    add functionality get all "value" fields to main

--- src/find_values_with/core.clj
+++ src/find_values_with/core.clj
@@ -1,9 +1,11 @@
 (ns find-values-with.core
-  (:require [cheshire.core :as json] [clojure.java.io])
+  (:require [cheshire.core :as json])
   (:gen-class))
 
 (defn -main
   "Returns parsed JSON from file, takes filepath string as input"
   [json-file-path]
   (let [get-parsed-json (json/parse-string (slurp json-file-path) true)]
-    get-parsed-json))
+    (map :value
+         (filter map?
+                 (tree-seq #(or (map? %) (vector? %)) identity (first get-parsed-json))))))
